8bcbe222eb397a797da519bc4b2f41fe1947fc45
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-07-29 17:09:21 +0200

    routes

--- index.js
+++ index.js
@@ -3,25 +3,79 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { User } = require("./models/User");
 const config = require("./config/keys");
+const users = require("./Routes/Users");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post("/api/users/register", (req, res) => {
-  const user = new User(req.body);
+app.use("/api/users", users);
 
-  user.save((err, userData) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({ success: true });
-  });
-});
+// app.get("/api/users/auth", auth, (req, res) => {
+//   res.status(200).json({
+//     _id: req.user.id,
+//     isAuth: true,
+//     email: req.user.email,
+//     name: req.user.name,
+//     lastName: req.user.lastName,
+//     role: req.user.role,
+//   });
+// });
 
-app.get("/", (req, res) => {
-  res.json("app running");
-});
+// app.post("/api/users/register", (req, res) => {
+//   const user = new User(req.body);
+
+//   user.save((err, doc) => {
+//     if (err) return res.json({ success: false, err });
+//     return res.status(200).json({ success: true, userData: doc });
+//   });
+// });
+
+// app.post("/api/users/login", (req, res) => {
+//   // find user
+//   //------------
+//   User.findOne({ email: req.body.email }, (err, user) => {
+//     if (!user)
+//       return res.json({
+//         loginSuccess: false,
+//         message: "auth failed ,email not found",
+//       });
+//     // compare password
+//     // -------------------
+//     user.comparePassword(req.body.password, (err, isMatch) => {
+//       if (!isMatch) {
+//         return res.json({ loginSuccess: false, message: "wrong password" });
+//       }
+//     });
+//     // generate token
+//     // -----------------
+//     user.generateToken((err, user) => {
+//       if (err) return res.status(400).send(err);
+//       res
+//         .cookie("x_auth", user.token)
+//         .status(200)
+//         .json({ loginSuccess: true, user });
+//     });
+//   });
+// });
+
+// LOGOUT
+//-----------
+
+// app.get("/api/users/logout", auth, (req, res) => {
+//   User.findOneAndUpdate(
+//     { useFindAndModify: false },
+//     { _id: req.user._id },
+//     { token: " " },
+//     (err, doc) => {
+//       if (err) return res.json({ success: false, err });
+//       return res.status(200).send({
+//         success: true,
+//       });
+//     }
+//   );
+// });
 
 mongoose
   .connect(config.mongoURI, {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,56 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// set hash before saving
+
+userSchema.pre("save", function (next) {
+  let user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  let user = this;
+
+  let token = jwt.sign(user._id.toHexString(), "secret");
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, "secret", function (err, decode) {
+    if (err) throw err;
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
